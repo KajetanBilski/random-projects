d28fd8cd608a5d495ea058a1f6047ca01529d18f
ProtossLuigi <protossluigi@gmail.com>
2023-03-29 23:26:16 +0200

    stole a lot of code

--- super-duper-mario/mario.py
+++ super-duper-mario/mario.py
@@ -1,15 +1,22 @@
-import sys
-import copy
-import numpy as np
+from collections import deque, namedtuple, OrderedDict
+from typing import Iterator, List, Tuple
 import torch
-from torch import nn
+from torch import Tensor, nn
+from torch.optim import Adam, Optimizer
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import IterableDataset
+from torchvision import transforms as T
+import numpy as np
+from pytorch_lightning import LightningModule, Trainer
+
 import gym
 from gym.spaces import Box
 from gym.wrappers import FrameStack
-from torchvision import transforms as T
+
 from nes_py.wrappers import JoypadSpace
+
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
 
 class SkipFrame(gym.Wrapper):
     def __init__(self, env, skip):
@@ -20,14 +27,13 @@ class SkipFrame(gym.Wrapper):
     def step(self, action):
         """Repeat action, and sum reward"""
         total_reward = 0.0
-        done = False
         for i in range(self._skip):
             # Accumulate reward and repeat the same action
-            obs, reward, done, info = self.env.step(action)
+            obs, reward, done, trunk, info = self.env.step(action)
             total_reward += reward
             if done:
                 break
-        return obs, total_reward, done, info
+        return obs, total_reward, done, trunk, info
 
 
 class GrayScaleObservation(gym.ObservationWrapper):
@@ -68,68 +74,333 @@ class ResizeObservation(gym.ObservationWrapper):
         return observation
 
 
-class Mario:
-    def __init__():
-        pass
+class DQN(nn.Module):
+    """Simple MLP network."""
 
-    def act(self, state):
-        """Given a state, choose an epsilon-greedy action"""
-        pass
-
-    def cache(self, experience):
-        """Add the experience to memory"""
-        pass
-
-    def recall(self):
-        """Sample experiences from memory"""
-        pass
-
-    def learn(self):
-        """Update online action value (Q) function with a batch of experiences"""
-        pass
-
-class MarioNet(nn.Module):
-    """mini cnn structure
-  input -> (conv2d + relu) x 3 -> flatten -> (dense + relu) x 2 -> output
-  """
-
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, obs_size: int, n_actions: int, hidden_size: int = 128):
+        """
+        Args:
+            obs_size: observation/state size of the environment
+            n_actions: number of discrete actions available in the environment
+            hidden_size: size of hidden layers
+        """
         super().__init__()
-        c, h, w = input_dim
-
-        if h != 84:
-            raise ValueError(f"Expecting input height: 84, got: {h}")
-        if w != 84:
-            raise ValueError(f"Expecting input width: 84, got: {w}")
-
-        self.online = nn.Sequential(
-            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=8, stride=4),
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(3136, 512),
-            nn.ReLU(),
-            nn.Linear(512, output_dim),
+            nn.Linear(hidden_size, n_actions),
         )
 
-        self.target = copy.deepcopy(self.online)
+    def forward(self, x):
+        return self.net(x.float())
 
-        # Q_target parameters are frozen.
-        for p in self.target.parameters():
-            p.requires_grad = False
 
-    def forward(self, input, model):
-        if model == "online":
-            return self.online(input)
-        elif model == "target":
-            return self.target(input)
+Experience = namedtuple(
+    "Experience",
+    field_names=["state", "action", "reward", "done", "new_state"],
+)
 
-def main(argv):
-    env = gym_super_mario_bros.make('SuperMarioBros-v0')
-    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+class ReplayBuffer:
+    """Replay Buffer for storing past experiences allowing the agent to learn from them.
+
+    Args:
+        capacity: size of the buffer
+    """
+
+    def __init__(self, capacity: int) -> None:
+        self.buffer = deque(maxlen=capacity)
+
+    def __len__(self) -> None:
+        return len(self.buffer)
+
+    def append(self, experience: Experience) -> None:
+        """Add experience to the buffer.
+
+        Args:
+            experience: tuple (state, action, reward, done, new_state)
+        """
+        self.buffer.append(experience)
+
+    def sample(self, batch_size: int) -> Tuple:
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, dones, next_states = zip(*(self.buffer[idx] for idx in indices))
+
+        return (
+            np.array(states),
+            np.array(actions),
+            np.array(rewards, dtype=np.float32),
+            np.array(dones, dtype=bool),
+            np.array(next_states),
+        )
+
+
+class RLDataset(IterableDataset):
+    """Iterable Dataset containing the ExperienceBuffer which will be updated with new experiences during training.
+
+    Args:
+        buffer: replay buffer
+        sample_size: number of experiences to sample at a time
+    """
+
+    def __init__(self, buffer: ReplayBuffer, sample_size: int = 200) -> None:
+        self.buffer = buffer
+        self.sample_size = sample_size
+
+    def __iter__(self) -> Iterator[Tuple]:
+        states, actions, rewards, dones, new_states = self.buffer.sample(self.sample_size)
+        for i in range(len(dones)):
+            yield states[i], actions[i], rewards[i], dones[i], new_states[i]
+
+
+class Agent:
+    """Base Agent class handeling the interaction with the environment."""
+
+    def __init__(self, env: gym.Env, replay_buffer: ReplayBuffer) -> None:
+        """
+        Args:
+            env: training environment
+            replay_buffer: replay buffer storing experiences
+        """
+        self.env = env
+        self.replay_buffer = replay_buffer
+        self.reset()
+        self.state, _ = self.env.reset()
+
+    def reset(self) -> None:
+        """Resents the environment and updates the state."""
+        self.state, _ = self.env.reset()
+
+    def get_action(self, net: nn.Module, epsilon: float, device: str) -> int:
+        """Using the given network, decide what action to carry out using an epsilon-greedy policy.
+
+        Args:
+            net: DQN network
+            epsilon: value to determine likelihood of taking a random action
+            device: current device
+
+        Returns:
+            action
+        """
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+        else:
+            state = torch.tensor([self.state])
+
+            if device not in ["cpu"]:
+                state = state.cuda(device)
+
+            q_values = net(state)
+            _, action = torch.max(q_values, dim=1)
+            action = int(action.item())
+
+        return action
+
+    @torch.no_grad()
+    def play_step(
+        self,
+        net: nn.Module,
+        epsilon: float = 0.0,
+        device: str = "cpu",
+    ) -> Tuple[float, bool]:
+        """Carries out a single interaction step between the agent and the environment.
+
+        Args:
+            net: DQN network
+            epsilon: value to determine likelihood of taking a random action
+            device: current device
+
+        Returns:
+            reward, done
+        """
+
+        action = self.get_action(net, epsilon, device)
+
+        # do step in the environment
+        new_state, reward, terminated, truncated, _ = self.env.step(action)
+        done = terminated or truncated
+
+        exp = Experience(self.state, action, reward, done, new_state)
+
+        self.replay_buffer.append(exp)
+
+        self.state = new_state
+        if done:
+            self.reset()
+        return reward, done
+
+
+class DQNLightning(LightningModule):
+    """Basic DQN Model."""
+
+    def __init__(
+        self,
+        batch_size: int = 16,
+        lr: float = 1e-2,
+        env: str = "CartPole-v0",
+        gamma: float = 0.99,
+        sync_rate: int = 10,
+        replay_size: int = 1000,
+        warm_start_size: int = 1000,
+        eps_last_frame: int = 1000,
+        eps_start: float = 1.0,
+        eps_end: float = 0.01,
+        episode_length: int = 200,
+        warm_start_steps: int = 1000,
+    ) -> None:
+        """
+        Args:
+            batch_size: size of the batches")
+            lr: learning rate
+            env: gym environment tag
+            gamma: discount factor
+            sync_rate: how many frames do we update the target network
+            replay_size: capacity of the replay buffer
+            warm_start_size: how many samples do we use to fill our buffer at the start of training
+            eps_last_frame: what frame should epsilon stop decaying
+            eps_start: starting value of epsilon
+            eps_end: final value of epsilon
+            episode_length: max length of an episode
+            warm_start_steps: max episode reward in the environment
+        """
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.env = gym.make(self.hparams.env)
+        obs_size = self.env.observation_space.shape[0]
+        n_actions = self.env.action_space.n
+
+        self.net = DQN(obs_size, n_actions)
+        self.target_net = DQN(obs_size, n_actions)
+
+        self.buffer = ReplayBuffer(self.hparams.replay_size)
+        self.agent = Agent(self.env, self.buffer)
+        self.total_reward = 0
+        self.episode_reward = 0
+        self.populate(self.hparams.warm_start_steps)
+
+    def populate(self, steps: int = 1000) -> None:
+        """Carries out several random steps through the environment to initially fill up the replay buffer with
+        experiences.
+
+        Args:
+            steps: number of random steps to populate the buffer with
+        """
+        for _ in range(steps):
+            self.agent.play_step(self.net, epsilon=1.0)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Passes in a state x through the network and gets the q_values of each action as an output.
+
+        Args:
+            x: environment state
+
+        Returns:
+            q values
+        """
+        output = self.net(x)
+        return output
+
+    def dqn_mse_loss(self, batch: Tuple[Tensor, Tensor]) -> Tensor:
+        """Calculates the mse loss using a mini batch from the replay buffer.
+
+        Args:
+            batch: current mini batch of replay data
+
+        Returns:
+            loss
+        """
+        states, actions, rewards, dones, next_states = batch
+
+        state_action_values = self.net(states).gather(1, actions.long().unsqueeze(-1)).squeeze(-1)
+
+        with torch.no_grad():
+            next_state_values = self.target_net(next_states).max(1)[0]
+            next_state_values[dones] = 0.0
+            next_state_values = next_state_values.detach()
+
+        expected_state_action_values = next_state_values * self.hparams.gamma + rewards
+
+        return nn.MSELoss()(state_action_values, expected_state_action_values)
+
+    def get_epsilon(self, start: int, end: int, frames: int) -> float:
+        if self.global_step > frames:
+            return end
+        return start - (self.global_step / frames) * (start - end)
+
+    def training_step(self, batch: Tuple[Tensor, Tensor], nb_batch) -> OrderedDict:
+        """Carries out a single step through the environment to update the replay buffer. Then calculates loss
+        based on the minibatch recieved.
+
+        Args:
+            batch: current mini batch of replay data
+            nb_batch: batch number
+
+        Returns:
+            Training loss and log metrics
+        """
+        device = self.get_device(batch)
+        epsilon = self.get_epsilon(self.hparams.eps_start, self.hparams.eps_end, self.hparams.eps_last_frame)
+        self.log("epsilon", epsilon)
+
+        # step through environment with agent
+        reward, done = self.agent.play_step(self.net, epsilon, device)
+        self.episode_reward += reward
+        self.log("episode reward", self.episode_reward)
+
+        # calculates training loss
+        loss = self.dqn_mse_loss(batch)
+
+        if done:
+            self.total_reward = self.episode_reward
+            self.episode_reward = 0
+
+        # Soft update of target network
+        if self.global_step % self.hparams.sync_rate == 0:
+            self.target_net.load_state_dict(self.net.state_dict())
+
+        self.log_dict(
+            {
+                "reward": reward,
+                "train_loss": loss,
+            }
+        )
+        self.log("total_reward", self.total_reward, prog_bar=True)
+        self.log("steps", self.global_step, logger=False, prog_bar=True)
+
+        return loss
+
+    def configure_optimizers(self) -> List[Optimizer]:
+        """Initialize Adam optimizer."""
+        optimizer = Adam(self.net.parameters(), lr=self.hparams.lr)
+        return optimizer
+
+    def __dataloader(self) -> DataLoader:
+        """Initialize the Replay Buffer dataset used for retrieving experiences."""
+        dataset = RLDataset(self.buffer, self.hparams.episode_length)
+        dataloader = DataLoader(
+            dataset=dataset,
+            batch_size=self.hparams.batch_size,
+        )
+        return dataloader
+
+    def train_dataloader(self) -> DataLoader:
+        """Get train loader."""
+        return self.__dataloader()
+
+    def get_device(self, batch) -> str:
+        """Retrieve device currently being used by minibatch."""
+        return batch[0].device.index if self.on_gpu else "cpu"
+
+
+def main():
+    env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0", render_mode='rgb', apply_api_compatibility=True)
+
+    env = JoypadSpace(env, [["right"], ["right", "A"]])
+
+    env.reset()
+    next_state, reward, done, trunc, info = env.step(action=0)
+    print(f"{next_state.shape},\n {reward},\n {done},\n {info}")
 
     # Apply Wrappers to environment
     env = SkipFrame(env, skip=4)
@@ -137,14 +408,5 @@ def main(argv):
     env = ResizeObservation(env, shape=84)
     env = FrameStack(env, num_stack=4)
 
-    done = True
-    for step in range(5000):
-        if done:
-            state = env.reset()
-        state, reward, done, info = env.step(env.action_space.sample())
-        env.render()
-
-    env.close()
-
-if __name__ == '__main__':
-    main(sys.argv)
+if __name__ == "__main__":
+    main()
